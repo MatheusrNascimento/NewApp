e7d7e7210856f5354dc6fdb309b2634dde73fad3
Matheus Nascimento <matheusrnascimento92@gmail.com>
2025-05-04 23:48:33 -0300

    Criando formulario e logica de validacao do formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'NewApp';
+export class AppComponent  implements OnInit {
+  title = 'Homes';
+  formPerson!: FormGroup;
+
+  ngOnInit() {
+    this.formPerson = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]),
+      nickname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+      birthdate: new FormControl('', [Validators.required],),
+      stack: new FormArray([
+        new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
+      ]),
+    });
+  }
+
+    sendData() {
+      if (this.formPerson.valid) {
+        console.log('Formulário enviado:', this.formPerson.value);
+    }
+  }
+
+  get stackControls() {
+    return (this.formPerson.get('stack') as FormArray).controls;
+  }
+
+  addStack() {
+    (this.formPerson.get('stack') as FormArray).push(new FormControl('', Validators.required));
+  }
+  
+  removeStack(index: number) {
+    (this.formPerson.get('stack') as FormArray).removeAt(index);
+  }
 }
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,17 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
+import { provideRouter } from '@angular/router';
 import { appConfig } from './app.config';
+import { routes } from './app.routes';
+
+import { provideHttpClient } from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
-  ]
+    provideRouter(routes),
+    provideServerRendering(),
+    provideHttpClient(),
+    ]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
